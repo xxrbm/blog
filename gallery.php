3f40a9bc53613ff71d9afa864e7610710aa6cf56
Wortel Dzgn <50727056+worteldzgn@users.noreply.github.com>
2020-07-10 20:16:45 +0700

    Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,6 @@
+.gallery {position:relative;margin:5px;margin-bottom:-3px;float:left;width:180px;}
+.gallery img {width:100%;height:auto;}
+.btn-download i{color:#fff;font-size:17px;position:absolute;right:0px;border-radius: 0 6px 0 6px;background-color:#00A3FE;padding:10px 8px;z-index:99}
+@media screen and (max-width: 980px) {
+.gallery {width:222px;}.btn-download i{font-size:25px;padding:14px 12px;}
+}
